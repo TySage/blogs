2a392b47aadebdb00266a8e2f4a8a3269d0c6872
Tyler Sage <stckrz@Tylers-MacBook-Pro-4.local>
2022-10-04 11:19:24 -0600

    Heroku stuffs

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rag!&g1cjra)7gd!mrq#a04x801b%uldjqtsild-gopa=v_mm6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
